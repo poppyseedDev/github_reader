50b99d6fcc14fce5f34d9ac8c58825d0682923c7
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-05-23 16:42:21 +0200

    fixing bugs

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-from create_embeddings import create_embeddings, create_pinecone_index, test_pinecone
-import streamlit as st
 from streamlit_interface import StreamlitInterface
-from process_gpt import chunk_data_into_smaller_docs
 
 def main():
     """Main function"""
-    documents = StreamlitInterface().start_interface()
-    texts = chunk_data_into_smaller_docs(documents)
+    StreamlitInterface().start_interface()
     
-    embeddings = create_embeddings()
-    create_pinecone_index(texts=texts, embeddings=embeddings)
-    test_pinecone()
--- streamlit_interface.py
+++ streamlit_interface.py
@@ -1,28 +1,35 @@
 import streamlit as st
 from process_gpt import git_repo_reader, load_docs, parse_into_nodes, add_to_docsstore, define_multiple_indexes, test_some_queries
+from process_gpt import chunk_data_into_smaller_docs
+from create_embeddings import create_embeddings, create_pinecone_index, test_pinecone
 
 class StreamlitInterface:
     def __init__(self):
-        self.git_url = None
-    
+        self.repo_owner = None
+        self.repo_name = None
+        
     def start_interface(self):
         st.title("💻🦾 Load a GitHub repository")
-        self.git_url = st.text_input("Enter the GitHub URL of the repository: ")
-        if self.git_url:
-            documents = self.load_repo_from_github()
-            return documents
+        git_url = st.text_input("Enter the GitHub URL of the repository: ")
+        if git_url:
+            # Parse the git URL
+            self.parse_git_url(git_url=git_url)
 
-    def parse_git_url(self):
+            documents = self.load_repo_from_github()
+            texts = chunk_data_into_smaller_docs(documents)
+    
+            embeddings = create_embeddings()
+            create_pinecone_index(texts=texts, embeddings=embeddings)
+            test_pinecone()
+
+    def parse_git_url(self, git_url):
         """Parse a git URL into the owner and repo name"""
-        (repo_owner, repo_name) = self.git_url.split("/")[-2:]
-        return (repo_owner, repo_name)
-        
+        (self.repo_owner, self.repo_name) = git_url.split("/")[-2:]        
 
     def load_repo_from_github(self):
         """Load a repository from GitHub"""
-        (repo_owner, repo_name) = self.parse_git_url(self.git_url)
-        st.write(f"Loading the repository {repo_name}...")
-        documents= git_repo_reader(repo_owner, repo_name)
+        st.write(f"Loading the repository {self.repo_name}...")
+        documents= git_repo_reader(self.repo_owner, self.repo_name)
         st.write("Repository loaded.")
         print(f'Number of documents: {len(documents)}')
         return documents
